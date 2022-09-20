51a73fb97757ad7cf4b07ad95d8ede1c26b0a9cf
MBounouar <MBounouar@users.noreply.github.com>
2022-09-20 08:45:43 +0200

    initial commit

--- src/dash_pyfolio/stats_summary.py
+++ src/dash_pyfolio/stats_summary.py
@@ -1,6 +1,7 @@
 import empyrical as ep
 import scipy as sp
 import pandas as pd
+import numpy as np
 
 from .portfolio_data import Portfolio
 from .stats import value_at_risk
@@ -68,3 +69,128 @@ def perf_stats(
 
     stats = stats.reset_index()
     return stats
+
+
+def get_max_drawdown_underwater(underwater):
+    """
+    Determines peak, valley, and recovery dates given an 'underwater'
+    DataFrame.
+
+    An underwater DataFrame is a DataFrame that has precomputed
+    rolling drawdown.
+
+    Parameters
+    ----------
+    underwater : pd.Series
+       Underwater returns (rolling drawdown) of a strategy.
+
+    Returns
+    -------
+    peak : datetime
+        The maximum drawdown's peak.
+    valley : datetime
+        The maximum drawdown's valley.
+    recovery : datetime
+        The maximum drawdown's recovery.
+    """
+
+    valley = underwater.idxmin()  # end of the period
+    # Find first 0
+    peak = underwater[:valley][underwater[:valley] == 0].index[-1]
+    # Find last 0
+    try:
+        recovery = underwater[valley:][underwater[valley:] == 0].index[0]
+    except IndexError:
+        recovery = np.nan  # drawdown not recovered
+    return peak, valley, recovery
+
+
+# def get_top_drawdowns(returns, top=10):
+def get_top_drawdowns(df_cum, top=10):
+    """
+    Finds top drawdowns, sorted by drawdown amount.
+
+    Parameters
+    ----------
+    returns : pd.Series
+        Daily returns of the strategy, noncumulative.
+         - See full explanation in tears.create_full_tear_sheet.
+    top : int, optional
+        The amount of top drawdowns to find (default 10).
+
+    Returns
+    -------
+    drawdowns : list
+        List of drawdown peaks, valleys, and recoveries. See get_max_drawdown.
+    """
+
+    running_max = np.maximum.accumulate(df_cum)
+    underwater = df_cum / running_max - 1
+
+    drawdowns = []
+    for _ in range(top):
+        peak, valley, recovery = get_max_drawdown_underwater(underwater)
+        # Slice out draw-down period
+        if not pd.isnull(recovery):
+            underwater.drop(underwater[peak:recovery].index[1:-1], inplace=True)
+        else:
+            # drawdown has not ended yet
+            underwater = underwater.loc[:peak]
+
+        drawdowns.append((peak, valley, recovery))
+        # if (len(returns) == 0) or (len(underwater) == 0) or (np.min(underwater) == 0):
+        if (len(underwater) == 0) or (np.min(underwater) == 0):
+            break
+
+    return drawdowns
+
+
+def top_drawdown_table(portfolio: Portfolio, top: int = 5) -> pd.DataFrame:
+    """Top Drawdown Table.
+
+    Args:
+        portfolio (Portfolio): _description_
+        top (int, optional): Number of drawdown periods to find. Defaults to 5.
+
+    Returns:
+        pd.DataFrame: drawdowns
+    """
+    returns = portfolio.returns[portfolio.portfolio_name]
+    df_cum = ep.cum_returns(returns, 1.0)
+    # drawdown_periods = get_top_drawdowns(returns, top=top)
+    drawdown_periods = get_top_drawdowns(df_cum, top=top)
+    df_drawdowns = pd.DataFrame(
+        index=range(top),
+        columns=[
+            "Net drawdown in %",
+            "Peak date",
+            "Valley date",
+            "Recovery date",
+            "Duration",
+        ],
+    )
+
+    for i, (peak, valley, recovery) in enumerate(drawdown_periods):
+        if pd.isnull(recovery):
+            df_drawdowns.loc[i, "Duration"] = np.nan
+        else:
+            df_drawdowns.loc[i, "Duration"] = len(
+                pd.date_range(peak, recovery, freq="B")
+            )
+        df_drawdowns.loc[i, "Peak date"] = peak.to_pydatetime().strftime("%Y-%m-%d")
+        df_drawdowns.loc[i, "Valley date"] = valley.to_pydatetime().strftime("%Y-%m-%d")
+        if isinstance(recovery, float):
+            df_drawdowns.loc[i, "Recovery date"] = recovery
+        else:
+            df_drawdowns.loc[i, "Recovery date"] = recovery.to_pydatetime().strftime(
+                "%Y-%m-%d"
+            )
+        df_drawdowns.loc[i, "Net drawdown in %"] = (
+            (df_cum.loc[peak] - df_cum.loc[valley]) / df_cum.loc[peak]
+        ) * 100
+
+    df_drawdowns["Peak date"] = pd.to_datetime(df_drawdowns["Peak date"])
+    df_drawdowns["Valley date"] = pd.to_datetime(df_drawdowns["Valley date"])
+    df_drawdowns["Recovery date"] = pd.to_datetime(df_drawdowns["Recovery date"])
+
+    return df_drawdowns
